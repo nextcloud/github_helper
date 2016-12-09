0797ff313fbeada890267e19d3d68690b82ca6e0
Lukas Reschke <lukas@statuscode.ch>
2016-12-09 12:18:19 +0100

    Use argv
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- tagger/tag.php
+++ tagger/tag.php
@@ -1,30 +1,57 @@
 <?php 
+if(count($argv) !== 3) {
+	die("tag.php \$branchname \$tag\n");
+}
+$branch = $argv[1];
+$tag = $argv[2];
 
-$branch = 'stable10';
-$tag = 'v10.0.1RC1';
-
-$repositories = [
-	'server',
-	'3rdparty',
-	'apps',
-	'files_pdfviewer',
-	'files_texteditor',
-	'files_videoplayer',
-	'gallery',
-	'apps',
-	'firstrunwizard',
-	'activity',
-	'templateeditor',
-	'notifications',
-	'password_policy',
-	'user_saml',
-	'files_accesscontrol',
-	'files_automatedtagging',
-	'files_retention',
-	'serverinfo',
-	'survey_client',
-	'example-files',
-];
+switch($branch) {
+	case 'stable9':
+		$repositories = [
+			'server',
+			'3rdparty',
+			'apps',
+			'files_pdfviewer',
+			'files_texteditor',
+			'files_videoplayer',
+			'gallery',
+			'apps',
+			'firstrunwizard',
+			'activity',
+			'templateeditor',
+			'notifications',
+			'password_policy',
+			'user_saml',
+			'example-files',
+		];
+		break;
+	case 'stable10':
+		$repositories = [
+			'server',
+			'3rdparty',
+			'apps',
+			'files_pdfviewer',
+			'files_texteditor',
+			'files_videoplayer',
+			'gallery',
+			'apps',
+			'firstrunwizard',
+			'activity',
+			'templateeditor',
+			'notifications',
+			'password_policy',
+			'user_saml',
+			'files_accesscontrol',
+			'files_automatedtagging',
+			'files_retention',
+			'serverinfo',
+			'survey_client',
+			'example-files',
+		];
+		break;
+	default:
+		die("Branch not found :(\n");
+}
 
 foreach($repositories as $repo) {
 	// Clone the repository
@@ -35,4 +62,6 @@ foreach($repositories as $repo) {
 	shell_exec('cd ' . __DIR__ . '/' . $repo . ' && git tag -s ' . $tag . ' -m \'' . $tag . '\'');
 	// Push the signed tag
 	shell_exec('cd ' . __DIR__ . '/' . $repo . ' && git push origin ' . $tag);
+	// Delete repository
+	shell_exec('cd ' . __DIR__ . ' && rm -rf ' . $repo)
 }
