2e34c22fee6826eddb13c090e5e0345a51b30401
Andy Scherzinger <info@andy-scherzinger.de>
2024-07-23 14:24:46 +0200

    feat(spdx-converter): support swift files
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- spdx-convertor/convert.php
+++ spdx-convertor/convert.php
@@ -106,6 +106,15 @@ function generateSpdxContent(string $originalHeader, string $file): array {
 			}
 			$authors[] = $m[2];
 
+		// Copyright (c) 2024 Joas Schilling <coding@schilljs.com>
+		} elseif (preg_match('/Copyright \(c\) (\d{4}),? ([^<]+) <([^>]+)>/', $line, $m)) {
+			if (str_contains(strtolower($m[2]), 'owncloud')) {
+				$newHeaderLines[] = "SPDX-FileCopyrightText: {$m[1]} {$m[2]} <{$m[3]}>";
+			} elseif ($nextcloudersCopyrightYear > $m[1]) {
+				$nextcloudersCopyrightYear = (int) $m[1];
+			}
+			$authors[] = "{$m[2]} <{$m[3]}>";
+
 		// @copyright 2023
 		} elseif (preg_match('/@copyright (\d{4})/', $line, $m)) {
 		} elseif (preg_match('/@author ([^\n]+)/', $line, $m)) {
@@ -120,11 +129,16 @@ function generateSpdxContent(string $originalHeader, string $file): array {
 			$license = 'SPDX-License-Identifier: AGPL-3.0-only';
 		} elseif (str_contains($line, 'This file is licensed under the Affero General Public License version 3 or')) {
 			$license = 'SPDX-License-Identifier: AGPL-3.0-or-later';
+		} elseif (str_contains($line, '// GNU GPL version 3 or any later version')) {
+			$license = 'SPDX-License-Identifier: GPL-3.0-or-later';
+		} elseif (str_contains($line, 'it under the terms of the GNU General Public License as published by')) {
 		} elseif (str_contains($line, 'it under the terms of the GNU Affero General Public License as')) {
 		} elseif (str_contains($line, 'it under the terms of the GNU Afferoq General Public License as')) {
 		} elseif (str_contains($line, 'it under the terms of the GNU Affero General Public License, version 3,')) {
 		} elseif (str_contains($line, 'License, or (at your option) any later version.')) {
+		} elseif (str_contains($line, 'GNU General Public License for more details.')) {
 		} elseif (str_contains($line, 'GNU Affero General Public License for more details.')) {
+		} elseif (str_contains($line, 'You should have received a copy of the GNU General Public License')) {
 		} elseif (str_contains($line, 'You should have received a copy of the GNU Affero General Public License')) {
 		} elseif (str_contains($line, 'the Free Software Foundation, either version 3 of the License, or')) {
 		} elseif (str_contains($line, 'along with this program.  If not, see <http://www.gnu.org/licenses/>')) {
@@ -263,6 +277,55 @@ function replaceVueCopyright(string $file, bool $isDryRun): array {
 	return $authors;
 }
 
+function replaceSwiftCopyright(string $file, bool $isDryRun): array {
+	$content = file_get_contents($file);
+
+	$headerStart = str_starts_with($content, '//') ? 0 : strpos($content, "\n//");
+	if ($headerStart === false) {
+		echo " ├─ ❌ \033[0;31m" . $file . ' No header comment found' . "\033[0m\n";
+		abortFurtherAnalysing();
+	}
+
+	$headerEndToken = "import";
+	$headerEnd = strpos($content, $headerEndToken, $headerStart);
+	if ($headerEnd === false) {
+		$headerEndToken = "extension";
+		$headerEnd = strpos($content, $headerEndToken, $headerStart);
+		if ($headerEnd === false) {
+			$headerEndToken = "protocol";
+			$headerEnd = strpos($content, $headerEndToken, $headerStart);
+			if ($headerEnd === false) {
+				$headerEndToken = "class";
+				$headerEnd = strpos($content, $headerEndToken, $headerStart);
+				if ($headerEnd === false) {
+					echo " ├─ ❌ \033[0;31m" . $file . ' No header comment END found' . "\033[0m\n";
+					abortFurtherAnalysing();
+				}
+			}
+		}
+	}
+
+	$originalHeader = substr($content, $headerStart, $headerEnd - $headerStart + strlen($headerEndToken));
+	if (str_contains($originalHeader, 'SPDX')) {
+		echo " ├─ ✅ \033[0;32m" . $file . ' SPDX' . "\033[0m\n";
+		return [];
+	}
+
+	[$authors, $newHeaderLines] = generateSpdxContent($originalHeader, $file);
+	$newHeader = (($headerStart === 0) ? '' : "\n") . "//\n// " . implode("\n// ", $newHeaderLines) . "\n//\n\n" . $headerEndToken;
+
+	if ($isDryRun) {
+		echo " ├─ ☑️  \033[0;36m" . $file . ' would replace' . "\033[0m\n";
+	} else {
+		file_put_contents(
+			$file,
+			str_replace($originalHeader, $newHeader, $content)
+		);
+		echo " ├─ ☑️  \033[0;36m" . $file . ' replaced' . "\033[0m\n";
+	}
+	return $authors;
+}
+
 $finder = new \Symfony\Component\Finder\Finder();
 $finder->ignoreVCSIgnored(true)
 	->sortByName();
@@ -325,6 +388,8 @@ foreach ($finder->getIterator() as $file) {
 		} else {
 			echo " ├─ 🔶 \033[0;33m" . $file->getRealPath() . ' skipped' . "\033[0m\n";
 		}
+	} elseif ($file->getExtension() === 'swift') {
+		$authors[] = replaceSwiftCopyright($file->getRealPath(), $isDryRun);
 	} elseif (!$file->isDir()) {
 		if (
 			str_ends_with($file->getRealPath(), 'composer.json')
